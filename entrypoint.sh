8397cc4bd4c1305a06b67f42b09d1e513d09bfe8
Saketh Chandra <b.sakethchandra9@gmail.com>
2021-08-06 18:34:28 +0530

    Now we can Create Super-User from Entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,7 @@
 python manage.py migrate --no-input
 python manage.py collectstatic --no-input
 
+DJANGO_SUPERUSER_PASSWORD=$SUPER_USER_PASSWORD python manage.py createsuperuser --username $SUPER_USER_NAME --email $SUPER_USER_EMAIL --noinput
+
 gunicorn django_project.wsgi:application --bind 0.0.0.0:8000
 
