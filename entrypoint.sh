30709977264f95859164a54739e13dda82456789
zzm88 <sea3@qq.com>
2021-12-09 15:14:55 +0800

    Update entrypoint.sh
    I removed --no-input  for the error( django_gunicorn_1  | manage.py migrate: error: unrecognized arguments: --no-input)
    and the "&&" is a solution for error ( django_gunicorn_1  | Unknown command: 'migrate\r'. Did you mean migrate? )

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-python manage.py migrate --no-input
-python manage.py collectstatic --no-input
+python manage.py migrate && python manage.py collectstatic &&
 
 gunicorn django_project.wsgi:application --bind 0.0.0.0:8000
 
